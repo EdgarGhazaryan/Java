ef38756b4fe16ec005b92896e243db2935e640f9
Ed <edgar.ghazaryan888@gmail.com>
2020-10-28 22:47:31 +0400

    adding homework.

---
+++ src/homeworks/First.java
@@ -0,0 +1,23 @@
+package homeworks;
+
+public class First {
+
+    public static void main(String[] args) {
+        byte a = 2;
+        System.out.println(a);
+        short c = 5;
+        System.out.println(c);
+        int b = 85;
+        System.out.println(b);
+        long d = 655L;
+        System.out.println(d);
+        float e = 4.05F;
+        System.out.println(e);
+        double f = 3.14;
+        System.out.println(f);
+        char g = 's';
+        System.out.println(g);
+        boolean h = true;
+        System.out.println(h);
+    }
+}
